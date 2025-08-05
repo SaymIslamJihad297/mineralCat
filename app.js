0a757f37a27ba828c42ea60aeae9812487804e39
Jihad Sheikh <kojn6193@gmail.com>
2025-08-06 01:54:14 +0600

    added x-reset-token

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning', 'x-refresh-token'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-reset-token', 'x-refresh-token'],
 }));
 
 const mongoose = require('mongoose');

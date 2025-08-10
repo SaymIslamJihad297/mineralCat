f6b7dd68717e084cc8b53e0726f0e6bd0ba0c576
Jihad Sheikh <kojn6193@gmail.com>
2025-08-10 06:24:34 +0600

    Update SectionalMockTest.routes.js

--- routes/mockTestRoutes/SectionalMockTest.routes.js
+++ routes/mockTestRoutes/SectionalMockTest.routes.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
-const { addSectionalMockTest, getAllSectionalMockTest, deleteSectionalMockTest } = require("../../controllers/mockTestControllers/sectionalMockTest.Controller");
+const { addSectionalMockTest, getAllSectionalMockTest, deleteSectionalMockTest, getSingleSectionalMockTest } = require("../../controllers/mockTestControllers/sectionalMockTest.Controller");
 const { isUserLoggedIn , isAdminUser} = require('../../middleware/middlewares');
 
 router.post('/add', isUserLoggedIn, isAdminUser, addSectionalMockTest);
 
 router.get('/getAll/:type', isUserLoggedIn, getAllSectionalMockTest);
+
+router.get('/getSingleSectionalMockTest/:id', isUserLoggedIn, getSingleSectionalMockTest);
 
 router.delete('/delete/:id', isUserLoggedIn, isAdminUser, deleteSectionalMockTest);
 
